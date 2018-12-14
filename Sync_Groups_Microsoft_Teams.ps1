9bb630b4239a5d734623488827defe7920421512
Kevin Conod <kconod@tlitmv.com>
2018-12-14 09:53:59 -0600

    Redo SyncGroups

--- Sync_Groups_Microsoft_Teams.ps1
+++ Sync_Groups_Microsoft_Teams.ps1
@@ -2,30 +2,9 @@
 # Author: Antoon Bouw  #
 
 # First let's check for prerequisites #
-function Confirm-Module ($Module) {
-    # If module is imported say that and do nothing
-    if (Get-Module | Where-Object {$_.Name -eq $Module}) {
-    } else {
-        # If module is not imported, but available on disk then import
-        if (Get-Module -ListAvailable | Where-Object {$_.Name -eq $Module}) {
-            Import-Module $Module -Verbose
-        } else {
-            # If module is not imported, not available on disk, but is in online gallery then install and import
-            if (Find-Module -Name $Module | Where-Object {$_.Name -eq $Module}) {
-                Install-Module -Name $Module -Force -Verbose -Scope CurrentUser
-                Import-Module $Module -Verbose
-            } else {
-                # If module is not imported, not available and not in online gallery then abort
-                write-host "Module $Module not imported, not available and not in online gallery, exiting."
-                exit 1
-            }
-        }
-    }
-}
 
 ## 1: NuGet (needed for installing the Teams module)
 $NuGetProvider = Get-PackageProvider -Name NuGet -Force
-
 if ($NuGetProvider.version -lt "2.8.5.201") {
 	Write-Output "Installing NuGet..."
 	Install-PackageProvider -Name NuGet -Scope AllUsers -Force
@@ -36,59 +15,61 @@ if ($NuGetProvider.version -lt "2.8.5.201") {
 
 ## 2: The Powershell Module for Teams
 $TeamsModule = Get-Module -ListAvailable -Name MicrosoftTeams
-
 if ($null -eq $TeamsModule) {
     Install-Module MicrosoftTeams -Scope AllUsers -Force 
 }
 
 ## 3: Active Directory Powershell Module
-
-## This one is a prerequisite and should be installed at all time. Make sure you use the prereq.ps1 file to install all neccesary functions
-
 $ADPowershell = Get-WindowsFeature -Name rsat-ad-powershell
-
 if ($ADPowershell.Installstate -ne "Installed") {
-    Write-Output "The Active Directory Powershell Module is not installed and cannot be installed automatically by this script."`n"Please Install Manually"
+	Write-Output "The Active Directory Powershell Module is not installed and cannot be installed automatically by this script."
+	Write-Output "Please run Install-ActiveDirectoryModule.ps1 as administrator"
     exit 1
 }
 
 # Let's get going #
 Import-Module ActiveDirectory
-$teams = import-csv Teams.csv -Header AD_Group,MS_Team
-$username = get-content UPN.txt
-$password = get-content cred.txt | convertto-securestring
-$credentials = new-object -typename System.Management.Automation.PSCredential -argumentlist $username,$password
-connect-microsoftteams -credential $credentials
+$ADGroupToTeamMap = Import-Csv Teams.csv -Header AD_Group, MS_Team
+$Username = Get-Content UPN.txt
+$Password = Get-Content cred.txt | ConvertTo-SecureString
+$Credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username, $Password
+Connect-MicrosoftTeams -Credential $Credentials
 
-Foreach ($team in $teams)
-{
-    $groupuserinfo = get-adgroup $team.AD_Group | get-adgroupmember | get-aduser | select-object UserPrincipalName
-    $teaminfo = get-team | where-object {$_.DisplayName -like $team.MS_Team}
-    if ($teaminfo -eq $null)
-	{
-		write-host "Team"$team.MS_Team"does not exist or Service Account is not owner"
+foreach ($Item in $ADGroupToTeamMap) {
+	# Get Active Directory group.
+	$ADGroup = Get-ADGroup $Item.AD_Group | Get-ADGroupMember | Get-ADUser | Select-Object UserPrincipalName
+	
+	# Check to see if Active Directory group maps to a Team.
+	if ($null -eq (Get-Team | Where-Object {$_.DisplayName -like $Item.MS_Team})) {
+		Write-Host "Team: " $Team.MS_Team " does not exist or Service Account is not owner"
 		break	
 	}
-    Else
-	{
-	    $teamuserinfo = get-teamuser -groupid $teaminfo.groupid
-	    $directoryonlyusers = try { compare-object -differenceobject $groupuserinfo.userprincipalname -referenceobject $teamuserinfo.user -includeequal -erroraction silentlycontinue | where-object {$_.SideIndicator -eq "=>"}} catch { write-host "Please add members to group"$team.AD_Group"Please make sure the owner is one of them!!!!" }
-	    foreach ($directoryonlyuser in $directoryonlyusers)
-		{
-			write-host "Adding User"$directoryonlyuser.InputObject"to Team"$teaminfo.displayname
-			add-teamuser -groupid $teaminfo.groupid -user $directoryonlyuser.inputobject
+    else {
+		# Get Team users
+		$Team = Get-TeamUser -GroupId $TeamInfo.groupid
+
+		# Gather a list of Active Directory users that do not belong to the Team
+	    $ADOnlyUsers = try {
+			Compare-Object -DifferenceObject $ADGroup.UserPrincipalName -ReferenceObject $Team.User -IncludeEqual -ErrorAction SilentlyContinue | Where-Object {$_.SideIndicator -eq "=>"}
+		} catch { Write-Host "Please add members to group " $team.AD_Group ". Please make sure the owner is one of them." }
+
+		# Add missing Active Directory users to Team
+	    foreach ($ADOnlyUser in $ADOnlyUsers) {
+			Write-Host "Adding User " $ADOnlyUser.InputObject " to Team " $Team.DisplayName
+			Add-TeamUser -GroupId $Team.groupid -User $ADOnlyUser.InputObject
 		}
-	    $teamonlyusers = try { compare-object -differenceobject $groupuserinfo.userprincipalname -referenceobject $teamuserinfo.user -includeequal -erroraction silentlycontinue | where-object {$_.SideIndicator -eq "<="}} catch {}
-	    foreach ($teamonlyuser in $teamonlyusers)		
-		{
-			write-host "Removing User"$teamonlyuser.InputObject"from Team"$teaminfo.displayname
-			$teamuserinfo = Get-TeamUser -GroupId $teaminfo.groupid | Where-Object {$_.User -like $teamonlyuser.inputobject}
-			if ($teamuserinfo.role -eq "Owner")
-				{
-					write-host "User"$teamonlyuser.InputObject"is not available in AD group, but cannot be removed from"$teaminfo.displayname"because it's an owner."
-					break
-				}
-			remove-teamuser -groupid $teaminfo.groupid -user $teamonlyuser.inputobject
+
+		# Gather a list of Team users that do not belong to the Active Directory group
+		$TeamOnlyUsers = try {
+			Compare-Object -DifferenceObject $ADGroup.UserPrincipalName -ReferenceObject $Team.User -IncludeEqual -ErrorAction SilentlyContinue | Where-Object {$_.SideIndicator -eq "<="}
+		} catch {}
+
+		# Remove Team users that do not belong to the Active Directory group
+	    foreach ($TeamOnlyUser in $TeamOnlyUsers) {
+			Write-Host "Removing User " $TeamOnlyUser.InputObject " from Team " $Team.DisplayName
+			$TeamUserInfo = Get-TeamUser -GroupId $TeamInfo.groupid | Where-Object {$_.User -like $TeamOnlyUser.InputObject}
+			if ($TeamUserInfo.Role -eq "Owner") { break }
+			Remove-TeamUser -GroupId $TeamInfo.groupid -User $TeamOnlyUser.InputObject
 		}
 	}
 }
